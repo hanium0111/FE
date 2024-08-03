7ed4a315de3f97d3eb782c461ec2ddfb2dd770f4
mim2013 <>
2024-08-03 22:04:46 +0900

    change design tourguide box

--- pages/components/TourGuide.js
+++ pages/components/TourGuide.js
@@ -12,8 +12,64 @@ const steps = [
   },
 ];
 
+const tourStyles = {
+  popover: (base) => ({
+    ...base,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderRadius: "10px",
+    padding: "30px",
+    maxWidth: "300px",
+  }),
+
+  arrow: (base, state) => ({
+    ...base,
+    color: "ccc",
+  }),
+
+  badge: (base) => ({
+    ...base,
+    backgroundColor: "#4629f2",
+    color: "#fff",
+  }),
+  button: (base, state) => ({
+    ...base,
+    color: "#fff",
+    background: "none #fff",
+    backgroundColor: "#fff",
+  }),
+  close: (base) => ({
+    ...base,
+    color: "#000",
+    background: "none #fff",
+    backgroundColor: "#fff",
+    top: "15px",
+    right: "15px",
+  }),
+  controls: (base) => ({
+    ...base,
+    buttonColor: "#fff",
+  }),
+  dot: (base, state) => ({
+    ...base,
+    backgroundColor: state.current ? "#4629f2" : "#ccc",
+  }),
+  maskArea: (base) => ({
+    ...base,
+    fill: "rgba(0, 0, 0, 0.9)",
+  }),
+  maskWrapper: (base) => ({
+    ...base,
+    opacity: 0.7,
+  }),
+};
+
 export const TourGuideProvider = ({ children }) => {
-  return <TourProvider steps={steps}>{children}</TourProvider>;
+  return (
+    <TourProvider steps={steps} styles={tourStyles}>
+      {children}
+    </TourProvider>
+  );
 };
 
 export const TourButton = () => {
