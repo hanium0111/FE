d2f06c30c1abfe9ae4451763553478c213362e85
lux00 <oys128950@gmail.com>
2024-08-08 20:24:35 +0900

    gen page query to using localStorage ;

--- pages/gen.js
+++ pages/gen.js
@@ -1,11 +1,25 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import styles from "../styles/Wrap.module.css";
 import GenerateBox from "../components/GenerateBox";
-import { useRouter } from "next/router";
 
 export default function Gen() {
   const router = useRouter();
-  const { projectPath } = router.query;
+  const [projectPath, setProjectPath] = useState("");
+
+  useEffect(() => {
+    const queryPath = router.query.projectPath;
+    if (queryPath) {
+      setProjectPath(queryPath);
+      localStorage.setItem("projectPath", queryPath);
+    } else {
+      const storedPath = localStorage.getItem("projectPath");
+      if (storedPath) {
+        setProjectPath(storedPath);
+      }
+    }
+  }, [router.query]);
 
   return (
     <div className={styles.wrap}>
