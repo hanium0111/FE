43046fb73d0fc873e22d9b812051eb0a3aceddbc
lux00 <oys128950@gmail.com>
2024-08-08 20:29:57 +0900

    rb

--- pages/gen.js
+++ pages/gen.js
@@ -1,25 +1,11 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import styles from "../styles/Wrap.module.css";
 import GenerateBox from "../components/GenerateBox";
+import { useRouter } from "next/router";
 
 export default function Gen() {
   const router = useRouter();
-  const [projectPath, setProjectPath] = useState("");
-
-  useEffect(() => {
-    const queryPath = router.query.projectPath;
-    if (queryPath) {
-      setProjectPath(queryPath);
-      localStorage.setItem("projectPath", queryPath);
-    } else {
-      const storedPath = localStorage.getItem("projectPath");
-      if (storedPath) {
-        setProjectPath(storedPath);
-      }
-    }
-  }, [router.query]);
+  const { projectPath } = router.query;
 
   return (
     <div className={styles.wrap}>
