4b815b7a352e600f02d84e21db9df1a5feed555b
lux00 <oys128950@gmail.com>
2024-08-13 16:09:24 +0900

    genTest update

--- components/GenerateBoxTest.js
+++ components/GenerateBoxTest.js
@@ -7,28 +7,37 @@ const GenerateBoxTest = ({ files }) => {
 
   useEffect(() => {
     const fetchFiles = async () => {
+      if (!files || !Array.isArray(files)) {
+        console.error("Files data is undefined or not an array");
+        return;
+      }
+
       const baseURL = "https://1am11m.store";
 
       const htmlFile = files.find((file) => file.name === "index.html");
       const cssFile = files.find((file) => file.name.endsWith(".css"));
       const jsFile = files.find((file) => file.name.endsWith(".js"));
 
-      if (htmlFile) {
-        const htmlResponse = await fetch(baseURL + htmlFile.path);
-        const htmlData = await htmlResponse.text();
-        setHtmlContent(htmlData);
-      }
+      try {
+        if (htmlFile) {
+          const htmlResponse = await fetch(baseURL + htmlFile.path);
+          const htmlData = await htmlResponse.text();
+          setHtmlContent(htmlData);
+        }
 
-      if (cssFile) {
-        const cssResponse = await fetch(baseURL + cssFile.path);
-        const cssData = await cssResponse.text();
-        setCssContent(cssData);
-      }
+        if (cssFile) {
+          const cssResponse = await fetch(baseURL + cssFile.path);
+          const cssData = await cssResponse.text();
+          setCssContent(cssData);
+        }
 
-      if (jsFile) {
-        const jsResponse = await fetch(baseURL + jsFile.path);
-        const jsData = await jsResponse.text();
-        setJsContent(jsData);
+        if (jsFile) {
+          const jsResponse = await fetch(baseURL + jsFile.path);
+          const jsData = await jsResponse.text();
+          setJsContent(jsData);
+        }
+      } catch (error) {
+        console.error("Error fetching files:", error);
       }
     };
 
@@ -63,12 +72,17 @@ const GenerateBoxTest = ({ files }) => {
 };
 
 export const getServerSideProps = async () => {
-  const response = await fetch(
-    "https://1am11m.store/user-templates/directory?dirPath=/copied_userTemplates/test_oys128950@gmail.com_1723447296518"
-  );
-  const files = await response.json();
+  try {
+    const response = await fetch(
+      "https://1am11m.store/user-templates/directory?dirPath=/copied_userTemplates/test_oys128950@gmail.com_1723447296518"
+    );
+    const files = await response.json();
 
-  return { props: { files } };
+    return { props: { files } };
+  } catch (error) {
+    console.error("Error fetching file list:", error);
+    return { props: { files: null } };
+  }
 };
 
 export default GenerateBoxTest;
