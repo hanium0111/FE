f5d0db045210f44bb7a66c1d6682fee67a6f1d09
lux00 <oys128950@gmail.com>
2024-08-14 17:58:44 +0900

    import Btn comp

--- components/GenerateBox.js
+++ components/GenerateBox.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "@/components/GenerateBox.module.css";
+import Btn from "./Btn";
 
 const GenerateBox = ({ projectPath }) => {
   const [indexFile, setIndexFile] = useState(null);
